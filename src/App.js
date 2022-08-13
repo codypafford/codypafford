bac59fee9d4e485a2854f93e8763700d2cd3e42d
Cody Pafford <codypafford6@gmail.com>
2022-08-13 19:58:46 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
+import Home from './components/Home';
+import NavigationMenu from "./components/NavigationMenu";
+import AboutMe from "./components/AboutMe";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavigationMenu/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about-me' element={<AboutMe/>}/>
+        <Route path='/clicker' element={<Game/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
