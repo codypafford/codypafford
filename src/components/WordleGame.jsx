5550455daa1fc1022143f733a87f35bc02ebb09f
Cody Pafford <codypafford6@gmail.com>
2022-08-15 11:57:48 -0400

    added more game stuff

--- src/components/WordleGame.jsx
+++ src/components/WordleGame.jsx
@@ -190,7 +190,6 @@ class WordleGame extends Component {
         >
           <div className="content">
             <Button
-              className="center-element-justified"
               style={{ margin: "10px" }}
               onClick={this.checkSolution}
               disabled={!this.maxLettersPicked()}
@@ -239,12 +238,10 @@ class WordleGame extends Component {
                     );
                   })}
             </div>
-            <div className="center-element-justified">
-              Letters chosen: {this.state.arrayOfLettersPicked}
-            </div>
-            <div id="answer_box" className="center-element-justified"></div>
+            <div>Letters chosen: {this.state.arrayOfLettersPicked}</div>
+            <div id="answer_box"></div>
 
-            <div id="gcontainer" className="center-element-justified"></div>
+            <div id="gcontainer"></div>
           </div>
         </div>
       </Fragment>
